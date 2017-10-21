4e0a152f7aa7489fddadca37e82c104c1c91ee6e
Siwei Wang <32981830+siweiwang24@users.noreply.github.com>
2017-10-21 11:19:08 -0400

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -5,5 +5,17 @@
 #include <string>
 
 int main() {
+  char word[36] = "";
+  
+  printf("Please enter the word to be guessed: ");
+  
+  scanf("%s", word);
+  
+  char user[1] = "";
+  int x;
+  
+  printf("Guess a letter");
+  
+  scanf("%s", user);
   
 }
