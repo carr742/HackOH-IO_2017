7215664116a1805f3a394e1f22f4701dd91d9580
Siwei Wang <32981830+siweiwang24@users.noreply.github.com>
2017-10-21 12:40:20 -0400

    Update hangman_siwei.cpp

--- hangman_siwei.cpp
+++ hangman_siwei.cpp
@@ -11,38 +11,48 @@ int main() {
 	char blank[] = "______";
 	printf("Six letter word");
 	
-	//Initialize the guessing loop
+	//Set guesses to 10 and intialize variables
 
-	int guess = 10, k;
+	int guess = 10, k, correct;
+	char letter;
+	
+	//Guessing loop
 
 	while (guess > 0) {
 
-		char letter;
-		int correct = 0;
+		printf("\nYou have %d guesses left.", guess);	//Show how many guesses are left
 
-		printf("\n%s", blank);
+		correct = 0;							//Value of correct is initially zero at the start
+
+		printf("\n%s", blank);					//Display the blank underscores array
 
 		//Guess a letter
 
 		printf("\nGuess a letter: ");
+		scanf(" %c", &letter);					//Put space before %c because whitespace crap
 
-		scanf("%c", &letter);
+		//See if letter is part of word array over a loop
 
 		for (k = 0; k <= 6; k++) {
+			//Testing each individual element
+
 			if (word[k] == letter) {
-				printf("\nCorrect letter!");
-				blank[k] = letter;
-				word[k] = '_';
-				correct = 1;
-				break;
+				printf("\nCorrect letter!");	//Display that the answer was correct
+				blank[k] = letter;				//Replace that element of blank with the letter
+				word[k] = '_';					//Replace the element in word with underscore
+				correct = 1;					//Make correct equal to 1 to show that a correct letter was guessed on this loop
+				break;							//If word is correct, exit the for loop
 			}
 
 		}
-
+		
 		if (correct == 0) {
-			printf("Incorrect letter!");
+
+			printf("\nIncorrect guess!");
 			guess--;
 		}
+
+		
 	}
 
 	system("pause");
